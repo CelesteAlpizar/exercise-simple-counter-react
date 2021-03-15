9797dd4ae8875191feb922c89a122ba8e11d3b02
María Celeste Alpízar Solano <maria.alpizar.72@gmail.com>
2021-03-15 22:26:35 +0000

    SimpleCounter

---
+++ src/js/icons.js
@@ -0,0 +1,25 @@
+// // Global fontAwesomeConfig
+
+// // 1) First you have to import the #fortawesome/fontawesome library
+
+// import fontawesome from "fortawesome/fontawesome";
+
+// // 2) Then you have to import every icon that you will use (when the icon say far Fa... the r of the far means regular and if say fas Fa... the s of the fas means solid, we also can see in the end of the URL if it say regular or solid)
+
+// import faClock from "fortawesome/fontawesome-free-regular/faClock";
+
+// // 3) Tell font-awesome that you want to replace your icons with SVGs (recomended for performance)
+
+// fontawesome.config = {
+// 	autoReplaceSvg: "nest"
+// };
+
+// // // 4) Add the icons into the font-awesome library
+
+// fontawesome.library.add(faClock);
+
+// // // 5) Place the icon tag anywhere you want to show instanceof. Ex: <i class="fas fa-check"></i>
+
+// // // 6)Always remember make the reference in the index that you're working to import the document where you make this process
+
+// // import "./icons.js";
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,64 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
+import "./icons.js";
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+// import { PageCounter } from "./component/home.js";
+
+function PageCounter(props) {
+	return (
+		<div className="CounterWrapper">
+			<div className="CounterIcon">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="six">{props.digitSix % 10}</div>
+			<div className="five">{props.digitFive % 10}</div>
+			<div className="four">{props.digitFour % 10}</div>
+			<div className="three">{props.digitThree % 10}</div>
+			<div className="two">{props.digitTwo % 10}</div>
+			<div className="one">{props.digitOne % 10}</div>
+		</div>
+	);
+}
+
+PageCounter.propTypes = {
+	digitSix: PropTypes.number,
+	digitFive: PropTypes.number,
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number
+};
+
+let counter = 0;
+setInterval(function() {
+	let six = Math.floor(counter / 100000);
+	let five = Math.floor(counter / 10000);
+	let four = Math.floor(counter / 1000);
+	let three = Math.floor(counter / 100);
+	let two = Math.floor(counter / 10);
+	let one = Math.floor(counter / 1);
+	console.log(six, five, four, three, two, one);
+	counter++;
+	ReactDOM.render(
+		<PageCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+			digitFive={five}
+			digitSix={six}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
